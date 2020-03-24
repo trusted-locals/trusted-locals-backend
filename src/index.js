49f0682cd48584026290c66a7ff83d1b84ce7257
Oliver Micke <olivermicke@outlook.com>
2020-03-24 21:25:05 +0100

    feat: enable CORS

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
 const express = require('express');
-const bodyParser =require('body-parser')
-const {registerUser,signIn} =require('./auth/auth')
-const passport=require("passport")
-const {getToken}=require("./helpers/helpers")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { registerUser, signIn } = require('./auth/auth');
+const passport = require('passport');
+const { getToken } = require('./helpers/helpers');
 require('../passport')(passport);
 
 const app = express();
 const port = 8080;
 
+const frontendUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.FRONTEND_URL
+    : process.env.DEV_FRONTEND_URL;
+
 app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: frontendUrl }));
 
-app.get( "/", ( req, res ) => {
-    res.send("Hello World")
-} );
-
-app.post("/user",(req,res)=>{
-    registerUser(req,res)
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.post("/user/login",(req,res)=>{
-    signIn(req,res)
-})
+app.post('/user', (req, res) => {
+  registerUser(req, res);
+});
 
-app.get("/test",passport.authenticate('jwt', { session: false}),(req,res)=>{
+app.post('/user/login', (req, res) => {
+  signIn(req, res);
+});
+
+app.get(
+  '/test',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
     var token = getToken(req.headers);
     if (token) {
-        res.send("authenticated successfully")
+      res.send('authenticated successfully');
     } else {
-        res.send("error")
+      res.send('error');
     }
-})
+  }
+);
 
-app.listen( port, () => {
-    console.log( `server started a http://localhost:${ port }` );
-} );
-
-
+app.listen(port, () => {
+  console.log(`server started a http://localhost:${port}`);
+});
